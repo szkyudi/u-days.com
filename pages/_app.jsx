b4f9348fcb1a2bd76d11bb2f43c0f78bb4dcb7ce
szkyudi <soccer.gk.yudai.s@gmail.com>
2020-12-23 19:26:40 +0900

    fix: font-familyの指定

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import { Global, css } from '@emotion/core'
 import destyle from 'destyle.css'
+import commonStyle from '../lib/styles/common.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Global styles={css`${destyle}`} />
+      <Global styles={css`${commonStyle}`} />
       <Component {...pageProps} />
     </>
   )
