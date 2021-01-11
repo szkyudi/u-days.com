b9910d6435b2f994a46f190fbe644e88ab54eacb
szkyudi <soccer.gk.yudai.s@gmail.com>
2021-01-11 21:09:50 +0900

    add: 読み込みバーの追加

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,25 @@
 import { Global, css } from '@emotion/core'
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import destyle from 'destyle.css'
 import commonStyle from '../lib/styles/common'
+import nprogressStyle from '../lib/styles/nprogress'
+
+NProgress.configure({
+    minimum: 0.2,
+    easing: 'ease',
+    speed: 400,
+    showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Global styles={css`${destyle}`} />
-      <Global styles={commonStyle} />
+      <Global styles={[css`${destyle}`, nprogressStyle, commonStyle]} />
       <Component {...pageProps} />
     </>
   )
