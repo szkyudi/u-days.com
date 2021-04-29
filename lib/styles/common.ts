30cbddb2849a9499e138f110912cd0e2fa36bd1b
szkyudi <soccer.gk.yudai.s@gmail.com>
2021-04-29 22:04:38 +0900

    update: 文字色の更新

--- lib/styles/common.ts
+++ lib/styles/common.ts
@@ -1,11 +1,13 @@
-import { css } from '@emotion/react'
-import variables from './variables'
+import { css } from "@emotion/react";
+import variables from "./variables";
 
 const common = css`
   body {
-    font-family: "Segoe UI", "Helvetica Neue", "Hiragino Kaku Gothic ProN", メイリオ, meiryo, sans-serif;
+    font-family: "Segoe UI", "Helvetica Neue", "Hiragino Kaku Gothic ProN",
+      メイリオ, meiryo, sans-serif;
     background: ${variables.color.background};
+    color: ${variables.color.textMain};
   }
-`
+`;
 
 export default common;
