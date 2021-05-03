7f3e703746e2ca476f2d80c7dea593f1f3011f5d
szkyudi <soccer.gk.yudai.s@gmail.com>
2021-05-03 11:51:38 +0900

    updata: 記事一覧の切り出し

--- pages/tags/[slug].tsx
+++ pages/tags/[slug].tsx
@@ -2,6 +2,7 @@ import { GetStaticProps, GetStaticPaths } from 'next'
 import { getTaggedPostsData } from '../../lib/posts'
 import { getAllTagsIds, getTagData } from '../../lib/tags'
 import TaggedPosts from '../../components/templates/TaggedPosts'
+import { getProfileData } from '../../lib/profile'
 
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -19,17 +20,20 @@ export const getStaticProps: GetStaticProps = async ({ params }: {
 }) => {
   const tag = await getTagData(params.slug)
   const posts = await getTaggedPostsData(tag.id)
+  const profile = await getProfileData()
   return {
     props: {
       tag,
-      posts
+      posts,
+      profile
     }
   }
 }
 
-export default function Home({ tag, posts }: {
-  tag: Tag
-  posts: Post[]
+export default function Home({ tag, posts, profile }: {
+  tag: Tag,
+  posts: Post[],
+  profile: Profile
 }) {
-  return <TaggedPosts tag={tag} posts={posts} />
+  return <TaggedPosts tag={tag} posts={posts} profile={profile} />
 }
