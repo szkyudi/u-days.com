f641ad50ae4d08784eed7cc58d48760b992a61b6
szkyudi <soccer.gk.yudai.s@gmail.com>
2021-05-03 09:54:37 +0900

    update: CSSの切り出し

--- components/organisms/Page.tsx
+++ components/organisms/Page.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react'
 import variables from '../../lib/styles/variables'
+import postContent from '../../lib/styles/postContent'
 
 const header = css`
   margin-bottom: ${variables.space.sm};
@@ -11,55 +12,7 @@ const title = css`
   line-height: 1.4;
 `
 
-const content = css`
-  line-height: 1.8;
-  h2, h3, h4, h5, h6 {
-    margin: 1.8em 0 .6em;
-    line-height: 1.5;
-    font-weight: bold;
-  }
-  h2 {
-    font-size: 24px;
-    border-bottom: 1px solid ${variables.color.textMuted};
-  }
-  h3 {
-    font-size: 20px;
-  }
-  h4 {
-    font-size: 16px;
-  }
-  p + p {
-    margin-top: .6em;
-  }
-  a {
-    color: ${variables.color.primaryVariant};
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-  ul, ol {
-    margin: 1em 0;
-    padding: 0 0 0 1.5em;
-  }
-  ul {
-    list-style: disc;
-  }
-  ol {
-    list-style: decimal;
-  }
-  strong, b {
-    font-weight: bold;
-  }
-  img {
-    display: block;
-    max-width: 100%;
-    max-height: 300px;
-    margin: 1em 0;
-    @media screen and (min-width: ${variables.breakpoints.md}px) {
-      max-height: 600px;
-    }
-  }
-`
+const content = postContent
 
 export default function Page({ page }: { page: Page }) {
   return (
