29fc8135b6d7c2bd04caec37ad93db63cd26208c
szkyudi <soccer.gk.yudai.s@gmail.com>
2021-05-03 11:51:38 +0900

    updata: 記事一覧の切り出し

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,20 +1,24 @@
 import { GetServerSideProps } from 'next'
 import SearchTemplate from '../components/templates/Search'
 import { getSortedPostsData } from '../lib/posts'
+import { getProfileData } from '../lib/profile'
 
 export const getServerSideProps: GetServerSideProps = async ({query}: {query: any}) => {
   const posts = await getSortedPostsData(query.keyword)
+  const profile = await getProfileData()
   return {
     props: {
+      keyword: query.keyword,
       posts,
-      keyword: query.keyword
+      profile
     }
   }
 }
 
-export default function Search({ posts, keyword }: {
-  posts:Post[]
-  keyword: string
+export default function Search({ keyword, posts, profile }: {
+  keyword: string,
+  posts:Post[],
+  profile: Profile
 }) {
-  return <SearchTemplate posts={posts} keyword={keyword} />
+  return <SearchTemplate keyword={keyword} posts={posts} profile={profile} />
 }
