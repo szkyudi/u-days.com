50069b2690ba0af30e207b81cd40af10830544c5
szkyudi <soccer.gk.yudai.s@gmail.com>
2022-02-21 23:35:02 +0900

    add: highlight.jsの導入

--- lib/pages.ts
+++ lib/pages.ts
@@ -1,4 +1,5 @@
 const contentful = require('contentful')
+import hljs from 'remark-highlight.js'
 import remark from 'remark'
 import html from 'remark-html'
 import { IPage } from '../@types/generated/contentful'
@@ -11,6 +12,7 @@ const client = contentful.createClient({
 export async function getPageData(id: string): Promise<IPage> {
   const page: IPage = await client.getEntry(id)
   const processedContent = await remark()
+    .use(hljs)
     .use(html)
     .process(page.fields.body)
   const convertedHtml = processedContent.toString()
--- lib/profile.ts
+++ lib/profile.ts
@@ -1,4 +1,5 @@
 const contentful = require("contentful");
+import hljs from "remark-highlight.js";
 import remark from "remark";
 import html from "remark-html";
 import { IProfile } from "../@types/generated/contentful";
@@ -12,7 +13,10 @@ const PROFILE_ID = "jJW1tzhmlClF1LXPv7AtD";
 
 export async function getProfileData(): Promise<IProfile> {
   const profile: IProfile = await client.getEntry(PROFILE_ID);
-  const processedContent = await remark().use(html).process(profile.fields.bio);
+  const processedContent = await remark()
+    .use(hljs)
+    .use(html)
+    .process(profile.fields.bio);
   const convertedHtml = processedContent.toString();
 
   profile.fields.bio = convertedHtml;
