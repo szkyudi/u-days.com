d54d0e739829e3362289709d60077f14bfd89c8f
szkyudi <soccer.gk.yudai.s@gmail.com>
2022-02-27 16:03:13 +0900

    fix: 不要な記述の削除

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,6 @@ import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/do
 import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return initialProps
-  }
-
   render() {
     return (
       <Html lang="ja">
