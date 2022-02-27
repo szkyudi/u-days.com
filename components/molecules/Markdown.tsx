9b870894f463cc61e87ff34a2b1f4cbe9af2d0ac
szkyudi <soccer.gk.yudai.s@gmail.com>
2022-02-27 14:28:39 +0900

    update: 自動外部リンク化

--- components/molecules/Markdown.tsx
+++ components/molecules/Markdown.tsx
@@ -1,11 +1,21 @@
 import ReactMarkdown from "react-markdown"
+import config from "../../lib/config"
 import { CodeBlock } from "./CodeBlock"
 
 export const Markdown = ({ children, ...props }) => {
   return (
     <ReactMarkdown
       components={{
-        code: CodeBlock
+        code: CodeBlock,
+        a: ({ href, ...props }) => {
+          const domain = config.hostname
+          const regexp = new RegExp(domain)
+          if (!regexp.test(href)) {
+            return <a href={href} target="_blank" rel="noreferrer noopener" {...props} />
+          } else {
+            return <a href={href} {...props} />
+          }
+        }
       }}
       {...props}
     >
--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,8 @@ const config = {
   title: 'U-DAYS',
   titleSeperator: '｜',
   subtitle: 'とあるWEBエンジニアのつぶやき',
-  description: 'とあるWEBエンジニアのブログ。主にWEBフロント系の記事を投稿します。'
+  description: 'とあるWEBエンジニアのブログ。主にWEBフロント系の記事を投稿します。',
+  hostname: 'u-days.com',
 }
 
 export default config
